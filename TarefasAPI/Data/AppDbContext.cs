4ce94458bcfbb9e44ceb0819b04cd13313a2485d
DrFaelSan <rafaelvplima@gmail.com>
2025-02-17 15:17:45 -0300

    feat[BE]
    - Adicionando Estruta de pastas iniciais
    - Adição do context - AppDbContext

---
+++ TarefasAPI/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TarefasAPI.Data.Entidades;
+
+namespace TarefasAPI.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Tarefa> Tarefas { get; set; } 
+}
---
+++ TarefasAPI/Data/Entidades/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TarefasAPI.Data.Entidades;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; } = null!;
+    public bool Completa { get; set; }
+}
